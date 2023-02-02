096c4a7d1bb519232dc5269283f14150fe606cbc
doccocaubai <gnhatminhg@gmail.com>
2023-02-02 21:45:21 +0700

    be v1

---
+++ be/qlnk/src/main/java/edu/homework/qlnk/QlnkApplication.java
@@ -0,0 +1,13 @@
+package edu.homework.qlnk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlnkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlnkApplication.class, args);
+	}
+
+}
---
+++ be/qlnk/src/test/java/edu/homework/qlnk/QlnkApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.homework.qlnk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlnkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
